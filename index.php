3fb30d4061338856f5b9868d74d3dcf300a3d30f
maurocen <mauro.centurion@fing.edu.uy>
2016-07-24 19:05:16 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="blog.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="header"></div>
+
+		<div class="content">
+			<div class="posts">
+			<replace></replace>
+			</div>
+		</div>
+
+	</body>
+</html>
