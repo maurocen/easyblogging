79bfdc944fb85fd756b7bca6fadc0923ec70899b
Mauro Centurion <mauro@centurionvirdo.com>
2016-08-16 13:17:22 +0000

    Moved add_user.php and add_post.php to a different folder and added edit_config.php

---
+++ bootstrap/resources/add_user.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php 
+		session_start();
+		header('Content-Type: text/html; charset=UTF-8');
+		
+		require_once("../Spyc.php");
+		require_once("../salt.php");
+		
+		$database = Spyc::YAMLload("../hash.yaml");
+
+		$u_New = $_POST['u_New'];
+		$p_New = $_POST['p_New'];	
+		
+		$from_form = $_POST['from_form'];
+
+		if (isset($_SESSION['name']) && ($u_New!=null) && (p_New!=null) && $from_form) {
+			
+			if ($database[$u_New]["salt"]) {
+			}
+			else {
+				$database[$u_New]["salt"] = salt();	
+				$database[$u_New]["hash"] = hash(sha512, $database[$u_New]["salt"].$p_New);	
+			}
+			
+			$hfile = fopen("../hash.yaml", 'w');
+			$yaml = Spyc::YAMLdump($database,4,PHP_INT_MAX);
+			
+			fwrite($hfile, $yaml);
+			header('Location: ../index.php');
+		}
+		else {
+			header('Location: ../index.php');
+		}
+	?>
+</body>
+</html>
---
+++ bootstrap/resources/edit_config.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php 
+		session_start();
+		header('Content-Type: text/html; charset=UTF-8');
+		
+		require_once("../Spyc.php");
+		require_once("../salt.php");
+		
+		$config = Spyc::YAMLload("../config.yaml");
+
+		$from_form = $_POST['from_form'];
+
+		if (isset($_SESSION['name']) && $from_form) {
+			if (isset($_POST['p_Title'])) {
+				$config["title"] = true;
+			}
+			else {
+				$config["title"] = false;
+			}
+			if (isset($_POST["p_Date"])) {
+				$config["date"] = true;
+			}
+			else {
+				$config["date"] = false;
+			}
+			if (isset($_POST['p_Time'])) {
+				$config["time"] = true;
+			}
+			else {
+				$config["time"] = false;
+			}
+			if (isset($_POST['p_Author'])) {
+				$config["author"] = true;
+			}
+			else {
+				$config["author"] = false;
+			}
+			
+			$hfile = fopen("../config.yaml", 'w');
+			$yaml = Spyc::YAMLdump($config,4,PHP_INT_MAX);
+			
+			fwrite($hfile, $yaml);
+			header('Location: ../index.php');
+		}
+		else {
+			header('Location: ../index.php');
+		}
+	?>
+</body>
+</html>
